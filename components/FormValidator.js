5e74151b362eb7f931834f4db7163543637596b6
Aleksandr Goldshtern <alexg020573@gmail.com>
2024-05-07 21:22:59 +0300

    refactor and modified: components/FormValidator.js

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -39,15 +39,22 @@ export default class FormValidator {
     return !inputList.every((inputEl) => inputEl.validity.valid);
   }
 
-  _toggleButtonState() {
-    if (this._hasInvalidInput(this._inputEls)) {
-      this._submitButton.classList.add(this._inactiveButtonClass);
-      this._submitButton.disabled = true;
-      return;
-    }
+  disableButton() {
+    this._submitButton.classList.add(this._inactiveButtonClass);
+    this._submitButton.disabled = true;
+  }
 
+  _enableButton() {
     this._submitButton.classList.remove(this._inactiveButtonClass);
     this._submitButton.disabled = false;
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput(this._inputEls)) {
+      this.disableButton();
+    } else {
+      this._enableButton();
+    }
   }
 
   _setEventListeners() {
